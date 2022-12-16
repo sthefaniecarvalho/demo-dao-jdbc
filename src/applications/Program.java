d3b2cc86330ba033e67b54cb8692c9971cdb5bea
Sthefanie <sthefaniegc06@gmail.com>
2022-12-16 18:00:11 -0300

    update implementation

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -43,6 +43,12 @@ public class Program {
 		
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id: " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setEmail("martha@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 
 	}
 
