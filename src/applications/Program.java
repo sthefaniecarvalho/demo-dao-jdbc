d8e9334fe348c087e5d0cd2474ace7b4cb827cb6
Sthefanie <sthefaniegc06@gmail.com>
2022-12-16 12:39:11 -0300

    Department entity class

---
+++ src/applications/Program.java
@@ -0,0 +1,14 @@
+package applications;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "book");
+		System.out.println(obj);
+
+	}
+
+}
